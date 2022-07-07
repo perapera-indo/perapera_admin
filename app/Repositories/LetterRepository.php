09f183cc71274db9e2fc43b75dd0809bc6025d03
fnfs <farhannfs5121@gmail.com>
2022-07-08 02:09:24 +0700

    upload letter

--- app/Repositories/LetterRepository.php
+++ app/Repositories/LetterRepository.php
@@ -3,53 +3,30 @@
 namespace App\Repositories;
 
 use App\Models\Letter;
-use App\Models\LetterCategory;
-use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
-use GuzzleHttp\Client;
 
 class LetterRepository
 {
     public function createNew($data)
     {
-        $client = new Client([
-            'base_uri' => env('CLOUD_S3_UPLOAD'),
-            // default timeout 5 detik
-            'timeout'  => 5,
-        ]);
         $letter = new Letter;
-        // for image
-        $image = base64_encode(file_get_contents($data['image_url']));
-        $response_img = $client->request('POST', '/api/v1/cdn', [
-            'json' => [
-                'bucket' => 'room1',
-                'image' => $image
-            ]
-        ]);
 
-        $body_img = $response_img->getBody();
-        $img_url = json_decode($body_img);
+        $url = $data['category']=="1" ? "uploads/katakana" : "uploads/hiragana";
 
-        if (isset($data['color_image_url'])) { // for image color
-            $image_color = base64_encode(file_get_contents($data['color_image_url']));
-            $response_img_color = $client->request('POST', '/api/v1/cdn', [
-                'json' => [
-                    'bucket' => 'room1',
-                    'image' => $image_color
-                ]
-            ]);
-            $body_img_color = $response_img_color->getBody();
-            $color_img_url = json_decode($body_img_color);
-
-            $letter->color_image_url = $color_img_url->data->path;
+        if (!empty($data['color_image_url'])) {
+            $uploadImage = upload_file($data['color_image_url'], $url, 'image');
+            $letter->color_image_url = $uploadImage['original'];
         }
 
+        if (!empty($data['image_url'])) {
+            $uploadImage = upload_file($data['image_url'], $url, 'image');
+            $letter->image_url = $uploadImage['original'];
+        }
 
 
         $letter->code = Str::random(10);
         $letter->letter = $data['letter'];
         $letter->romanji = $data['romanji'];
-        $letter->image_url = $img_url->data->path;
         $letter->total_stroke = $data['total_stroke'];
         $letter->letter_category_id = $data['category'];
         $letter->is_active = 1;
@@ -62,52 +39,19 @@ class LetterRepository
     public function updateLetter($data, $id)
     {
 
-
-        $client = new Client([
-            'base_uri' => env('CLOUD_S3_UPLOAD'),
-            // default timeout 5 detik
-            'timeout'  => 5,
-        ]);
-
         $letter = Letter::find($id);
 
-        if (isset($data['image_url'])) {
-            if ($letter->image_url != "" || $letter->image_url != "") {
-                $response_img_del = $client->request('DELETE', '/api/v1/cdn/' . $letter->image_url);
-            }
+        $url = $data['category']=="1" ? "uploads/katakana" : "uploads/hiragana";
 
-            // for image
-            $image = base64_encode(file_get_contents($data['image_url']));
-            $response_img = $client->request('POST', '/api/v1/cdn', [
-                'json' => [
-                    'bucket' => 'room1',
-                    'image' => $image
-                ]
-            ]);
-            $body_img = $response_img->getBody();
-            $img_url = json_decode($body_img);
-            $letter->image_url = $img_url->data->path;
+        if (!empty($data['color_image_url'])) {
+            $uploadImage = upload_file($data['color_image_url'], $url, 'image');
+            $letter->color_image_url = $uploadImage['original'];
         }
 
-        if (isset($data['color_image_url'])) {
-            if ($letter->color_image_url != "" || $letter->color_image_url != "") {
-                $response_img_col = $client->request('DELETE', '/api/v1/cdn/' . $letter->color_image_url);
-            }
-
-            // for image color
-            $image_color = base64_encode(file_get_contents($data['color_image_url']));
-            $response_img_color = $client->request('POST', '/api/v1/cdn', [
-                'json' => [
-                    'bucket' => 'room1',
-                    'image' => $image_color
-                ]
-            ]);
-
-            $body_img_color = $response_img_color->getBody();
-            $color_img_url = json_decode($body_img_color);
-            $letter->color_image_url = $color_img_url->data->path;
+        if (!empty($data['image_url'])) {
+            $uploadImage = upload_file($data['image_url'], $url, 'image');
+            $letter->image_url = $uploadImage['original'];
         }
-
 
         $letter->letter = $data['letter'];
         $letter->romanji = $data['romanji'];
